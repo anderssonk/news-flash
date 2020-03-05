b15fc95a911e502c614f42387c771224e2587f01
agyllang <46810941+agyllang@users.noreply.github.com>
2020-03-05 15:07:29 +0100

    feat(NewsFeed,NewsView)
    api render to NewsView,
    render function should be moved to some newsModel.. but is placed in NewsFeed for now.
    created a simple display in newsView, this shoud probably be done in ArticleDisplay

--- src/components/NewsFeed/NewsFeed.js
+++ src/components/NewsFeed/NewsFeed.js
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./NewsFeed.css";
 import NewsFeedView from "./NewsFeedView";
 
 const NewsFeed = () => {
-	return (
-		<div>
-			<NewsFeedView />
-		</div>
-	);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true); //TODO use to display spinner
+
+  useEffect(() => {
+    getDataFromAPITopHeadlines("everything", "se", "bernie sanders"); //  "everything" for search in API needs searchString, "top-headlines" for headlines needs country code,
+    // setIsLoading(false);
+  }, [loading]);
+
+  const getDataFromAPITopHeadlines = (type, country = "se", searchString) => {
+    //arguments used to change type of API call, type ="everything" uses searchString="some search string ", type="top-headlines" uses country="us,uk,se etc..."
+    //returns a promise
+
+    function handleHTTPError(response) {
+      if (response.ok) return response;
+      throw Error(response.statusText);
+    }
+    var url = "";
+
+    type === "everything" //to handle change between "everything" or "top-headlines"
+      ? (url = `http://newsapi.org/v2/${type}?q=${searchString}&apiKey=ad625da18cbf4029bf53d20dc9fdc5de`)
+      : (url =
+          `http://newsapi.org/v2/${type}?` +
+          `country=${country}&` +
+          "apiKey=ad625da18cbf4029bf53d20dc9fdc5de");
+
+    var req = new Request(url);
+
+    return fetch(req)
+      .then(handleHTTPError)
+      .then(response => response.json())
+      .then(response => {
+        setData(response.articles);
+      })
+      .catch(console.error);
+  };
+
+  return (
+    <div>
+      Hello NewsFeed
+      <NewsFeedView news={data} />
+    </div>
+  );
 };
 
 export default NewsFeed;
--- src/components/NewsFeed/NewsFeedView.js
+++ src/components/NewsFeed/NewsFeedView.js
@@ -1,7 +1,24 @@
 import React from "react";
 
-const NewsFeedView = () => {
-	return <div>NewsFeedView</div>;
+const NewsFeedView = ({ news }) => {
+  const displayArticle = article => {
+    return (
+      <div className="articleDisplay" key={article.url}>
+        <h3>{article.title}</h3>
+
+        <h5>{article.source.name}</h5>
+        <img className="articleImg" src={article.urlToImage} />
+        <p>published at : {article.publishedAt}</p>
+      </div>
+    );
+  };
+
+  return (
+    <div className="App">
+      Hello NewsFeedView
+      {news.map(article => displayArticle(article))}
+    </div>
+  );
 };
 
 export default NewsFeedView;
