b3735863837f1c14c226effbc6305ea8a6fb1a12
agyllang <46810941+agyllang@users.noreply.github.com>
2020-03-16 11:36:07 +0100

    feat NewsModel: added "addToStarred"/"removeFromStarred" and "addToFeed" features

--- src/components/NewsFeed/NewsFeed.js
+++ src/components/NewsFeed/NewsFeed.js
@@ -3,31 +3,36 @@ import NewsFeedView from "./NewsFeedView";
 import { ModelContext } from "../../NewsContext";
 
 const NewsFeed = () => {
-	const { model } = useContext(ModelContext);
-	const [searchResultState, setSearchResultState] = useState(null);
-	const [countryState, setCountryState] = useState("se");
-	const [textState, setTextState] = useState("");
-	const [typeState, setTypeState] = useState("top-headlines");
+  const { model } = useContext(ModelContext);
+  const [searchResultState, setSearchResultState] = useState(null);
+  const [countryState, setCountryState] = useState("se");
+  const [textState, setTextState] = useState("");
+  const [typeState, setTypeState] = useState("top-headlines");
 
-	const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-	useEffect(() => {
-		updateSearchResults();
-	}, []);
+  useEffect(() => {
+    updateSearchResults();
+  }, []);
 
-	const updateSearchResults = () => {
-		setIsLoading(true);
-		model.searchNews(typeState, countryState).then(data => {
-			setSearchResultState(data);
-			setIsLoading(false);
-		});
-	};
+  const updateSearchResults = () => {
+    setIsLoading(true);
+    model.searchNews(typeState, countryState).then(data => {
+      setSearchResultState(data);
+      data.map(article => {
+        article.id = article.url;
+      }); //sätter ett ID på varje article
+      model.addToFeed(data);
 
-	return (
-		<div>
-			<NewsFeedView className="newsfeed" news={searchResultState} />
-		</div>
-	);
+      setIsLoading(false);
+    });
+  };
+
+  return (
+    <div>
+      <NewsFeedView className="newsfeed" news={searchResultState} />
+    </div>
+  );
 };
 
 export default NewsFeed;
