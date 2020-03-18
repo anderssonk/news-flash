7e15406a1cd49c16a0012e022203b33063017747
agyllang <46810941+agyllang@users.noreply.github.com>
2020-03-18 17:07:14 +0100

    style: early stage of grid implementation
    setting up grid templates

--- src/components/NewsFeed/NewsFeed.js
+++ src/components/NewsFeed/NewsFeed.js
@@ -1,42 +1,38 @@
 import React, { useContext, useState, useEffect } from "react";
 import NewsFeedView from "./NewsFeedView";
-import Search from "./NewsSearch"; // Drop down:en
-import NewsCategories from "./NewsCategories";
 
 import { ModelContext } from "../../NewsContext";
 
 const NewsFeed = () => {
-	const { model } = useContext(ModelContext);
-	const [searchResultState, setSearchResultState] = useState(null);
-	const [countryState, setCountryState] = useState("se");
-	const [textState, setTextState] = useState("");
-	const [typeState, setTypeState] = useState("top-headlines");
-	const [categoryState, setCategoryState] = useState("general");
+  const { model } = useContext(ModelContext);
+  const [searchResultState, setSearchResultState] = useState(null);
+  const [countryState, setCountryState] = useState("se");
+  const [textState, setTextState] = useState("");
+  const [typeState, setTypeState] = useState("top-headlines");
+  const [categoryState, setCategoryState] = useState("general");
 
-	const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-	useEffect(() => {
-		updateSearchResults();
-	}, [countryState, categoryState]);
+  useEffect(() => {
+    updateSearchResults();
+  }, [countryState, categoryState]);
 
-	const updateSearchResults = () => {
-		setIsLoading(true);
-		model.searchNews(typeState, countryState, categoryState).then(data => {
-			setSearchResultState(data);
-			data.map(article => model.addToFeed(article));
-			setIsLoading(false);
-		});
-	};
+  const updateSearchResults = () => {
+    setIsLoading(true);
+    model.searchNews(typeState, countryState, categoryState).then(data => {
+      setSearchResultState(data);
+      data.map(article => model.addToFeed(article));
+      setIsLoading(false);
+    });
+  };
 
-	return (
-		<div>
-			<Search country={countrycode => setCountryState(countrycode)} />
-			<NewsCategories
-				setCategory={categoryState => setCategoryState(categoryState)}
-			/>
-			<NewsFeedView className="newsContainer" news={searchResultState} />
-		</div>
-	);
+  return (
+    <NewsFeedView
+      news={searchResultState}
+      country={countrycode => setCountryState(countrycode)}
+      category={categoryState => setCategoryState(categoryState)}
+    />
+  );
 };
 
 export default NewsFeed;
--- src/components/NewsFeed/NewsSearch.js
+++ src/components/NewsFeed/NewsSearch.js
@@ -1,34 +1,33 @@
 import React from "react";
 
 const NewsSearch = ({ country }) => {
-	const countries = {
-		se: "Sweden",
-		de: "Germany",
-		fr: "France",
-		gb: "United Kingdom",
-		no: "Norway",
-		us: "United States"
-	};
-	const countryCodes = Object.keys(countries);
-	const countryNames = Object.values(countries);
+  const countries = {
+    se: "Sweden",
+    de: "Germany",
+    fr: "France",
+    gb: "United Kingdom",
+    no: "Norway",
+    us: "United States"
+  };
+  const countryCodes = Object.keys(countries);
+  const countryNames = Object.values(countries);
 
-	const chooseCountry = countryCodes.map((
-		country //JSX element option
-	) => (
-		<option key={country} value={country}>
-			{" "}
-			{countryNames[countryCodes.indexOf(country)]}
-		</option>
-	));
-	return (
-		<div>
-			<form>
-				<select onChange={option => country(option.target.value)}>
-					{chooseCountry}
-				</select>
-			</form>
-		</div>
-	);
+  const chooseCountry = countryCodes.map((
+    country //JSX element option
+  ) => (
+    <option key={country} value={country}>
+      {countryNames[countryCodes.indexOf(country)]}
+    </option>
+  ));
+  return (
+    <div>
+      <form>
+        <select onChange={option => country(option.target.value)}>
+          {chooseCountry}
+        </select>
+      </form>
+    </div>
+  );
 };
 
 export default NewsSearch;
