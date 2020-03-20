e83ee7ed903a02e8a6a4a9d3782d17fd5f84fdc5
Kristina Andersson <kristina.andersson@mail.ru>
2020-03-20 12:11:43 +0100

    refactor: created a component-folder called search for search-related components

--- src/components/NewsFeed/NewsFeedView.js
+++ src/components/NewsFeed/NewsFeedView.js
@@ -1,22 +1,22 @@
 import React from "react";
 import ArticleDisplay from "../ArticleDisplay/ArticleDisplay";
-import Search from "./NewsSearch"; // Drop down:en
-import NewsCategories from "./NewsCategories";
+import Search from "../search/NewsSearch"; // Drop down:en
+import NewsCategories from "../search/NewsCategories";
 
 const NewsFeedView = ({ news, country, category }) => {
-  //console.log("News feed (NewsFeedView.js):", news);
-  return (
-    news && (
-      <div className="mainContent">
-        <h3>NewsFeedView</h3>
-        <Search country={country} />
-        <NewsCategories category={category} />
-        {news.map(article => (
-          <ArticleDisplay article={article} />
-        ))}
-      </div>
-    )
-  );
+	//console.log("News feed (NewsFeedView.js):", news);
+	return (
+		news && (
+			<div className="mainContent">
+				<h3>NewsFeedView</h3>
+				<Search country={country} />
+				<NewsCategories category={category} />
+				{news.map(article => (
+					<ArticleDisplay article={article} />
+				))}
+			</div>
+		)
+	);
 };
 
 export default NewsFeedView;
