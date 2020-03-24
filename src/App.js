ca162d6405dcfb3ff66bfbf2764b63b3ed90cb03
billyjan <billyjan@kth.se>
2020-03-24 10:15:37 +0100

    Firebase
    Firebase

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import "./App.css";
 import NewsFeed from "./components/NewsFeed/NewsFeed";
 import ModelContextProvider from "./NewsContext";
 import Sidebar from "./components/Sidebar/Sidebar";
+import firebaseConfig from "./util/firebaseConfig";
 
 function App() {
+  const firebase = require('firebase/app')
+  firebase.initializeApp(firebaseConfig);
   return (
     <div className="container">
       <ModelContextProvider>
