e1877f01f27ce3f197bb4518bc6c05d3d4d3f946
Kristina Andersson <kristina.andersson@mail.ru>
2020-04-02 17:03:33 +0200

    style: some dropdown-button styling

--- src/components/search/NewsSearch.js
+++ src/components/search/NewsSearch.js
@@ -1,33 +1,36 @@
 import React from "react";
 
 const NewsSearch = ({ country }) => {
-  const countries = {
-    se: "Sweden",
-    de: "Germany",
-    fr: "France",
-    gb: "United Kingdom",
-    no: "Norway",
-    us: "United States"
-  };
-  const countryCodes = Object.keys(countries);
-  const countryNames = Object.values(countries);
+	const countries = {
+		se: "SV",
+		de: "DE",
+		fr: "FR",
+		gb: "UK",
+		no: "NO",
+		us: "US"
+	};
+	const countryCodes = Object.keys(countries);
+	const countryNames = Object.values(countries);
 
-  const chooseCountry = countryCodes.map((
-    country //JSX element option
-  ) => (
-    <option key={country} value={country}>
-      {countryNames[countryCodes.indexOf(country)]}
-    </option>
-  ));
-  return (
-    <div>
-      <form>
-        <select onChange={option => country(option.target.value)}>
-          {chooseCountry}
-        </select>
-      </form>
-    </div>
-  );
+	const chooseCountry = countryCodes.map((
+		country //JSX element option
+	) => (
+		<option className="poop" key={country} value={country}>
+			{countryNames[countryCodes.indexOf(country)]}
+		</option>
+	));
+	return (
+		<div>
+			<form>
+				<select
+					className="dropdown"
+					onChange={option => country(option.target.value)}
+				>
+					{chooseCountry}
+				</select>
+			</form>
+		</div>
+	);
 };
 
 export default NewsSearch;
