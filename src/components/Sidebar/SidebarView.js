3dbfb3f3ded18ae5dcd23335b3675b840e36447c
agyllang <46810941+agyllang@users.noreply.github.com>
2020-04-02 14:37:21 +0200

    starredDisplay: anchor target blank

--- src/components/Sidebar/SidebarView.js
+++ src/components/Sidebar/SidebarView.js
@@ -1,24 +1,24 @@
 import React from "react";
 
 const SidebarView = ({ starred, remove }) => {
-	const starredDisplay = starredArray => {
-		return starredArray.map(article => (
-			<div className="starredDisplay" key={article.url}>
-				<div id="starredTitle">{article.title}</div>
-				<a href="article.url">
-					<div id="starredSource">{article.source.name}</div>
-				</a>
-				<button onClick={() => remove(article.url)}>X</button>
-			</div>
-		));
-	};
+  const starredDisplay = starredArray => {
+    return starredArray.map(article => (
+      <div className="starredDisplay" key={article.url}>
+        <div id="starredTitle">{article.title}</div>
+        <a href={article.url} target="_blank">
+          <div id="starredSource">{article.source.name}</div>
+        </a>
+        <button onClick={() => remove(article.url)}>X</button>
+      </div>
+    ));
+  };
 
-	return (
-		<div>
-			<h4>STARRED ARTICLES</h4>
-			{starred ? starredDisplay(starred) : "no starred articles"}
-		</div>
-	);
+  return (
+    <div>
+      <h4>STARRED ARTICLES</h4>
+      {starred ? starredDisplay(starred) : "no starred articles"}
+    </div>
+  );
 };
 
 export default SidebarView;
