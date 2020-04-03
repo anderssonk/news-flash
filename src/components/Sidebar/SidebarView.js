686fccfa1ddebefd9d584312911798ac39693de7
agyllang <46810941+agyllang@users.noreply.github.com>
2020-04-03 14:49:56 +0200

    styling materialize.css and button show and hide starred articles

--- src/components/Sidebar/SidebarView.js
+++ src/components/Sidebar/SidebarView.js
@@ -1,25 +1,39 @@
 import React from "react";
 
 const SidebarView = ({ starred, remove }) => {
-	const starredDisplay = starredArray => {
-		return starredArray.map(article => (
-			<div className="starredDisplay" key={article.url}>
-				<div id="starredTitle">{article.title}</div>
-				<a href={article.url} target="_blank" rel="noopener noreferrer">
-					<div id="starredSource">{article.source.name}</div>
-				</a>
-				<button onClick={() => remove(article.url)}>X</button>
-			</div>
-		));
-	};
+  const hideOrShow = () => {
+    var x = document.getElementById("starredDisplayList");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  };
+  const starredDisplay = starredArray => {
+    return starredArray.map(article => (
+      <div className="starredDisplay" key={article.url}>
+        <div id="starredTitle">{article.title}</div>
+        <a href={article.url} target="_blank" rel="noopener noreferrer">
+          <div id="starredSource">{article.source.name}</div>
+        </a>
+        <button onClick={() => remove(article.url)}>X</button>
+      </div>
+    ));
+  };
 
-	return (
-		<div>
-			<h4>Starred Articles</h4>
-			<div className="horisontal-line"></div>
-			{starred ? starredDisplay(starred) : "no starred articles"}
-		</div>
-	);
+  return (
+    <div>
+      <h4>Starred Articles</h4>
+      <button onClick={hideOrShow}>
+        <i class="material-icons">arrow_drop_down</i>
+      </button>
+      <div id="starredDisplayList">
+        <div className="horisontal-line"></div>
+
+        {starred ? starredDisplay(starred) : "no starred articles"}
+      </div>
+    </div>
+  );
 };
 
 export default SidebarView;
