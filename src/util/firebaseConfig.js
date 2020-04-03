6e88a10e9cc020efc04dfe3b248e48ef2ed49654
Kristina Andersson <kristina.andersson@mail.ru>
2020-04-03 14:09:55 +0200

    chore: removed util from repository

--- src/util/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyAG-FLhyeAjBajQpPqfH7SzCdu49V6_SbY",
-    authDomain: "newsflash-8e21c.firebaseapp.com",
-    databaseURL: "https://newsflash-8e21c.firebaseio.com",
-    projectId: "newsflash-8e21c",
-    storageBucket: "newsflash-8e21c.appspot.com",
-    messagingSenderId: "836289226955",
-    appId: "1:836289226955:web:fdb6f40e6bf9ef0874534e"
-  };
