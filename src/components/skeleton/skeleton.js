6c1b8ba2e5c6b04dce4cc90b7ef0b43158de7fca
annagu3 <annagu3@kth.se>
2020-04-08 21:56:59 +0200

    Initial skeleton

--- src/components/skeleton/skeleton.js
+++ src/components/skeleton/skeleton.js
@@ -1,13 +1,21 @@
 import React from "react";
 
 import "./skeletonstyle.css";
+import "../../App.css";
+import ArticleDisplay from "../articledisplay/ArticleDisplay";
+
+// background-repeat:repeat-y eller repeat-x
 
 const Skeleton = () => {
   return (
-    <div>
-      <div className="articleDisplay"></div>
-      <div className="articleDisplay"></div>
-      <div className="articleDisplay"></div>
+    <div className="mainContent">
+      <div className="generated-news">
+        <div className="skeletonDisplay articleDisplay"></div>
+        <div className="skeletonDisplay articleDisplay"></div>
+        <div className="skeletonDisplay articleDisplay"></div>
+        <div className="skeletonDisplay articleDisplay"></div>
+        <div className="skeletonDisplay articleDisplay"></div>
+      </div>
     </div>
   );
 };
