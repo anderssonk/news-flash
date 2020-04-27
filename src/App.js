7d1cd3a534bf4e4d2222e04607a766daa8ada384
billyjan <billyjan@kth.se>
2020-04-27 17:11:09 +0200

    firebase auth
    login with google

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import "./typography.css";
-import NewsFeed from "./components/newsFeed/NewsFeed";
-import ModelContextProvider from "./NewsContext";
-import Sidebar from "./components/sidebar/Sidebar";
-import Header from "./components/Header";
+//import NewsFeed from "./components/newsFeed/NewsFeed";
+//import ModelContextProvider from "./NewsContext";
+//import Sidebar from "./components/sidebar/Sidebar";
+//import Header from "./components/Header";
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
 function App() {
+	const [isSignedIn, setIsSignedIn] = useState(false)
+	const uiConfig = {
+		signInFlow: "popup",
+		signInOptions: [
+		  firebase.auth.GoogleAuthProvider.PROVIDER_ID
+		], callbacks : {
+		  signInSuccess: () => false
+		}
+	  }
 	return (
-		<body className="container">
-			<Header></Header>
+		<div className="container">
 			<div className="App">
-				<ModelContextProvider>
-					<NewsFeed />
-					<Sidebar />
-				</ModelContextProvider>
+				{isSignedIn ? 
+				<p>Signed in!</p>
+				:
+				<p>Not signed in!</p>
+				}	
 			</div>
-		</body>
+		</div>
 	);
 }
 
 export default App;
+
+// <ModelContextProvider>
+//<NewsFeed />
+					//<Sidebar />
+				//</ModelContextProvider>
