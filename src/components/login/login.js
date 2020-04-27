c4d7fa34d50fd549d959a2743a2fd2a3ab7cf861
agyllang <46810941+agyllang@users.noreply.github.com>
2020-04-27 19:33:26 +0200

    login function

---
+++ src/components/login/login.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import "./login.css";
+// import "./typography.css";
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAG-FLhyeAjBajQpPqfH7SzCdu49V6_SbY",
+  authDomain: "newsflash-8e21c.firebaseapp.com",
+});
+function Login() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+    callbacks: {
+      signInSuccess: () => false,
+    },
+  };
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      console.log("user", user);
+      setIsSignedIn(!!user);
+    });
+  });
+  return (
+    <div className="login">
+      {isSignedIn ? (
+        <div className="loggedIn">
+          <button onClick={() => firebase.auth().signOut()}>Sign out</button>
+          <p>Welcome {firebase.auth().currentUser.displayName}</p>
+          <img
+            id="profileImage"
+            alt="profileIMG"
+            src={firebase.auth().currentUser.photoURL}
+          />
+        </div>
+      ) : (
+        <div className="preLoggedIn">
+          <StyledFirebaseAuth
+            uiConfig={uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Login;
