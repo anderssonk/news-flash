922e7cd34245ea8affd64701284d43540498da70
ANNA GUSTAVSSON <annalovisaellen@gmail.com>
2020-04-29 09:44:56 +0200

    Responsive skeleton loader

--- src/components/skeletons/skeleton/skeleton.js
+++ src/components/skeletons/skeleton/skeleton.js
@@ -1,15 +1,9 @@
 import React from "react";
 
 import "./skeletonstyle.css";
-// import "../../../App.css";
-
-// const skeletonRepeats = Array(9).fill(0);
 
 const Skeleton = () => {
   return <div className="skeletonDisplay"></div>;
-  // return skeletonRepeats.map((index) => (
-  //   <div className="skeletonDisplay articleDisplay"></div>
-  // ));
 };
 
 export default Skeleton;
