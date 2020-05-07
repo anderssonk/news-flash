9c7dd7bdad1833ad3c22a2d4d52a000563e8f747
agyllang <46810941+agyllang@users.noreply.github.com>
2020-05-07 22:41:41 +0200

    css styling with login and article display

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -6,61 +6,61 @@ import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import { ModelContext } from "../../NewsContext";
 
 const Login = () => {
-	const { model } = useContext(ModelContext);
-	const db = firebase.firestore();
+  const { model } = useContext(ModelContext);
+  const db = firebase.firestore();
 
-	const [isSignedIn, setIsSignedIn] = useState(false);
-	const uiConfig = {
-		signInFlow: "popup",
-		signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
-		callbacks: {
-			signInSuccess: () => false,
-		},
-	};
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+    callbacks: {
+      signInSuccess: () => false,
+    },
+  };
 
-	async function starredPromise(user) {
-		var starred_array = [];
+  async function starredPromise(user) {
+    var starred_array = [];
 
-		var fetchData = db
-			.collection("users")
-			.doc(user.uid)
-			.collection("starred_collection")
-			.get();
+    var fetchData = db
+      .collection("users")
+      .doc(user.uid)
+      .collection("starred_collection")
+      .get();
 
-		var data = await fetchData;
-		const handleData = () => {
-			data.forEach((doc) => {
-				starred_array = starred_array.concat(doc.data().article);
-			});
-			return starred_array;
-		};
+    var data = await fetchData;
+    const handleData = () => {
+      data.forEach((doc) => {
+        starred_array = starred_array.concat(doc.data().article);
+      });
+      return starred_array;
+    };
 
-		var result = handleData();
-		return result;
-	}
-	useEffect(() => {
-		firebase.auth().onAuthStateChanged((user) => {
-			model.assignUser(user); //assigns the user in the model
+    var result = handleData();
+    return result;
+  }
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      model.assignUser(user); //assigns the user in the model
 
-			if (model.retrieveUserInfo()) {
-				//checks if user logged in
-				starredPromise(user) //loads saved articles from firestore
-					.then((result) => {
-						model.loginSetStarred(result); //populate sidebar with "this.starred" in model with saved articles
-					})
-					.catch((err) => {
-						console.log(
-							"There was an error retrieving data from firestore",
-							err
-						);
-					});
-			} else {
-				model.logoutSetStarred(); // resets the sidebar to empty by clearing the "this.starred" in model
-			}
+      if (model.retrieveUserInfo()) {
+        //checks if user logged in
+        starredPromise(user) //loads saved articles from firestore
+          .then((result) => {
+            model.loginSetStarred(result); //populate sidebar with "this.starred" in model with saved articles
+          })
+          .catch((err) => {
+            console.log(
+              "There was an error retrieving data from firestore",
+              err
+            );
+          });
+      } else {
+        model.logoutSetStarred(); // resets the sidebar to empty by clearing the "this.starred" in model
+      }
 
-			setIsSignedIn(!!user); // !!
-		});
-	}, []);
+      setIsSignedIn(!!user); // !!
+    });
+  }, []);
 
   return (
     <>
@@ -71,8 +71,16 @@ const Login = () => {
             alt="profileIMG"
             src={firebase.auth().currentUser.photoURL}
           />
-          <div id="loggedIn_message">Welcome {firebase.auth().currentUser.displayName}</div>
-          <button id ="logout_button" onClick={() => firebase.auth().signOut()}>Sign out</button>
+          <div id="loggedIn_message">
+            Welcome {firebase.auth().currentUser.displayName}
+          </div>
+          <button
+            className="btn-signout"
+            id="logout_button"
+            onClick={() => firebase.auth().signOut()}
+          >
+            <span>Sign out</span>
+          </button>
         </div>
       ) : (
         <div className="preLoggedIn">
@@ -84,6 +92,6 @@ const Login = () => {
       )}
     </>
   );
-}
+};
 
 export default Login;
